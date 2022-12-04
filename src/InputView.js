9b89cd1c7c602a8ed79cd1a133622d56ee640d2f
cruelladevil <dev.timetravel@gmail.com>
2022-12-04 16:37:16 +0900

    feat: throw Error with invalid input and retry to get input

--- src/InputView.js
+++ src/InputView.js
@@ -1,15 +1,37 @@
 const { Console } = require('@woowacourse/mission-utils');
+const OutputView = require('./OutputView');
+const Validation = require('./Validation');
 
 const InputView = {
   CAR_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   MOVE_COUNT: '시도할 회수는 몇회인가요?\n',
 
   readCarNames(callback) {
-    Console.readLine(InputView.CAR_NAMES, callback);
+    InputView.question(
+      InputView.CAR_NAMES,
+      Validation.validateCarNames,
+      callback,
+    );
   },
 
   readMoveCount(callback) {
-    Console.readLine(InputView.MOVE_COUNT, callback);
+    InputView.question(
+      InputView.MOVE_COUNT,
+      Validation.validateMoveCount,
+      callback,
+    );
+  },
+
+  question(string, validateFn, callback) {
+    Console.readLine(string, (input) => {
+      try {
+        validateFn(input);
+        callback(input);
+      } catch (error) {
+        OutputView.printError(error);
+        InputView.question(string, validateFn, callback);
+      }
+    });
   },
 };
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -3,6 +3,7 @@ const { Console } = require('@woowacourse/mission-utils');
 const OutputView = {
   RESULT_HEADER: '\n실행 결과',
   WINNERS: '최종 우승자 : ',
+  ERORR_HEADER: '[ERROR] ',
 
   printResultHeader() {
     Console.print(OutputView.RESULT_HEADER);
@@ -15,6 +16,10 @@ const OutputView = {
   printWinners(winnersViewData) {
     Console.print(`${OutputView.WINNERS} ${winnersViewData}`);
     Console.close();
+  },
+
+  printError(error) {
+    Console.print(`${OutputView.ERORR_HEADER}${error.message}`);
   },
 };
 
