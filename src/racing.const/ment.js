70455e56b374d89d45fedb6865cb269332959d0c
yeongi <cbg5255@gmail.com>
2022-12-04 13:57:00 +0900

    feat : const 멘트 상수 생성

---
+++ src/racing.const/ment.js
@@ -0,0 +1,16 @@
+const GAME = {
+  INPUT: {
+    CAR_LIST:
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+    COUNT: "시도할 회수는 몇회인가요?\n",
+  },
+  OUTPUT: {
+    MOVE_RESULT: "실행 결과\n",
+    WINNER: "최종 우승자 :",
+  },
+  RESULT: {
+    MOVE: "-",
+  },
+};
+
+module.export = GAME;
