ef30dbb31773713272daf5cc4261f36d7731ee5a
yeongi <cbg5255@gmail.com>
2022-12-04 17:21:43 +0900

    refactor : 예외멘트 상수 처리

--- src/racing.domain/InputExecption.js
+++ src/racing.domain/InputExecption.js
@@ -2,22 +2,28 @@ const RACING_GAME = require("../racing.const/game");
 
 const exception = (ment) => new Error(`[ERROR] ${ment}`);
 
+const MENT = {
+  MIN_CAR_COUNT: "차는 최소 2개 이상이여야 합니다.",
+  MAX_CAR_NAME: "차의 이름은 5 글자 이하여야 합니다.",
+  ONLY_NUMBER_INPUT: "숫자만 입력 가능합니다.",
+  INPUT_POSITIVE_INTEGER: "1 이상의 정수만 입력 가능합니다.",
+};
+
 const InputException = {
   checkNameList(listStr) {
     nameArr = listStr.split(",");
     console.log(nameArr, nameArr.length);
-    if (nameArr.length < 2) throw exception("차는 최소 2개 이상이여야 합니다.");
+    if (nameArr.length < 2) throw exception(MENT.MIN_CAR_COUNT);
     nameArr.map((name) => {
-      if (name.length > 5)
-        throw exception("차의 이름은 5 글자 이하여야 합니다.");
+      if (name.length > 5) throw exception(MAX_CAR_NAME);
     });
   },
 
   checkGameCount(count) {
     const number = Number(count);
-    if (Number.isNaN(number)) throw exception("숫자만 입력 가능합니다.");
+    if (Number.isNaN(number)) throw exception(MENT.ONLY_NUMBER_INPUT);
     if (number < RACING_GAME.INPUT_CONUT.MIN)
-      throw exception("1 이상의 정수만 입력 가능합니다.");
+      throw exception(INPUT_POSITIVE_INTEGER);
   },
 };
 
